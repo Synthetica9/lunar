bd5cbfa31b6b4474a7155d35e284e19279d33ab0
Patrick Hilhorst <git@hilhorst.be>
2022-11-18 14:39:53 +0100

    eval from perspective of current player, not from white

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -13,20 +13,23 @@ pub fn good_bishop(game: &Game) -> Millipawns {
     let bonus_per_pawn = Millipawns(20);
 
     let board = game.board();
+    let to_move = game.to_move();
 
     let mut score = Millipawns(0);
-    let white = board.get_color(&Color::White);
+    let ours = board.get_color(&to_move);
     let pawns = board.get_piece(&Piece::Pawn);
     for sq in board.get_piece(&Piece::Bishop).iter_squares() {
-        let multiplier = if white.get(sq) { 1 } else { -1 };
+        let multiplier = if ours.get(sq) { 1 } else { -1 };
 
         let is_light = sq.is_light();
-        let is_white = white.get(sq);
-        let our_pawns = pawns & white.flip_if(!is_white);
+        let is_ours = ours.get(sq);
+        let white_to_move = to_move == Color::White;
+        let is_white = is_ours == white_to_move;
+        let our_pawns = pawns & ours.flip_if(!is_ours);
 
         // Only count pawns in front of the bishop.
         let relevant_squares = if is_white {
-            !Bitboard::n_rows((sq.rank().as_index() as i8))
+            !Bitboard::n_rows(sq.rank().as_index() as i8)
         } else {
             !Bitboard::n_rows(-(8 - sq.rank().as_index() as i8))
         };
@@ -49,30 +52,38 @@ pub fn good_bishop(game: &Game) -> Millipawns {
 #[test]
 fn test_good_bishop() {
     // White has a good bishop, black has an especially bad one.
-    let game = Game::from_fen("b3k3/1p6/1Pp5/2Pp4/3Pp3/1B2P3/8/4K3 w - - 0 1").unwrap();
+    let mut game = Game::from_fen("b3k3/1p6/1Pp5/2Pp4/3Pp3/1B2P3/8/4K3 w - - 0 1").unwrap();
 
     let score = good_bishop(&game);
     println!("Score: {:?}", score);
 
     assert!(score > Millipawns(0));
+
+    game.make_move("Kf1").unwrap();
+
+    let score = good_bishop(&game);
+    println!("Score: {:?}", score);
+
+    assert!(score < Millipawns(0));
 }
 
 pub fn rook_on_open_file(game: &Game) -> Millipawns {
     let bonus_per_rook_half = Millipawns(100);
+    let to_move = game.to_move();
 
     let board = game.board();
     let mut score = Millipawns(0);
-    let white = board.get_color(&Color::White);
+    let ours = board.get_color(&to_move);
     let pawns = board.get_piece(&Piece::Pawn);
 
     for sq in board.get_piece(&Piece::Rook).iter_squares() {
-        let multiplier = if white.get(sq) { 1 } else { -1 };
+        let multiplier = if ours.get(sq) { 1 } else { -1 };
 
         let is_light = sq.is_light();
-        let is_white = white.get(sq);
+        let is_ours = ours.get(sq);
 
         let file = sq.file().as_bitboard();
-        let friendly_pawns = pawns & white.flip_if(!is_white);
+        let friendly_pawns = pawns & ours.flip_if(!is_ours);
 
         let open_file = (file & pawns).is_empty();
         let half_open_file = (file & friendly_pawns).is_empty();
@@ -86,12 +97,18 @@ pub fn rook_on_open_file(game: &Game) -> Millipawns {
 
 #[test]
 fn test_rook_on_open_file() {
-    let game = Game::from_fen("r3k2r/ppppp1pp/8/8/8/8/PPP1P1PP/3RKR2 w kq - 0 1").unwrap();
+    let mut game = Game::from_fen("r3k2r/ppppp1pp/8/8/8/8/PPP1P1PP/3RKR2 w kq - 0 1").unwrap();
 
     let score = rook_on_open_file(&game);
     println!("Score: {:?}", score);
 
     assert!(score > Millipawns(0));
+
+    game.make_move("a3").unwrap();
+
+    let score = rook_on_open_file(&game);
+    println!("Score: {:?}", score);
+    assert!(score < Millipawns(0));
 }
 
 pub fn evaluation(game: &Game) -> Millipawns {
