537dcb5c31c515615aea4940c4a6db41077ba53c
Patrick Hilhorst <git@hilhorst.be>
2022-11-23 17:38:06 +0100

    faster time control, better output for self-play

--- selfplay.py
+++ selfplay.py
@@ -8,15 +8,15 @@ import sys
 
 
 def selfplay(old, new):
-    old_name, old_path = old
-    new_name, new_path = new
+    old_rev, old_path = old
+    new_rev, new_path = new
 
     with setup_chess_cli() as cli:
         subprocess.check_call(
             [
                 str(cli),
-                *("-engine", f"name=lunar new ({new_name})", f"cmd={new_path}"),
-                *("-engine", f"name=lunar old ({old_name})", f"cmd={old_path}"),
+                *("-engine", f"name=lunar new - {name(new_rev)}", f"cmd={new_path}"),
+                *("-engine", f"name=lunar old - {name(old_rev)}", f"cmd={old_path}"),
                 # Set opening book
                 *(
                     "-openings",
@@ -24,16 +24,30 @@ def selfplay(old, new):
                     "order=random",
                     "-repeat",
                 ),
-                *("-each", "tc=20+1", "option.Hash=128"),
+                *("-each", "tc=2+.1", "option.Hash=128"),
                 *("-resign", "score=1000", "count=10"),
                 *("-draw", "number=40", "score=50", "count=20"),
                 *("-pgn", "out.pgn"),
                 *("-sprt",),
                 *("-concurrency", "2"),
-                *("-games", "100"),
+                *("-games", "1000"),
             ],
             stderr=subprocess.DEVNULL,
         )
+
+
+def rev_parse(rev):
+    return subprocess.check_output(["git", "rev-parse", rev]).strip().decode()
+
+
+def name(rev):
+    return (
+        subprocess.check_output(
+            ["git", "show", "--no-patch", "--format=format:%h (%s)", rev]
+        )
+        .strip()
+        .decode()
+    )
 
 
 @contextmanager
@@ -60,6 +74,13 @@ def setup_chess_cli():
 def main():
     if len(sys.argv) != 3:
         print("Usage: selfplay.py <old> <new>")
+        sys.exit(1)
+
+    old = rev_parse(sys.argv[1])
+    new = rev_parse(sys.argv[2])
+    if old == new:
+        print("These are the same commit!")
+        print("Both:", name(old))
         sys.exit(1)
 
     with compile_rev(sys.argv[1]) as old, compile_rev(sys.argv[2]) as new:
