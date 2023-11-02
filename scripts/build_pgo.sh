ebab3bd4d7b87b91154868885c84e20c47bf6924
Patrick Hilhorst <git@hilhorst.be>
2023-11-02 19:50:39 +0100

    use cargo-pgo

--- scripts/build_pgo.sh
+++ scripts/build_pgo.sh
@@ -2,24 +2,20 @@
 
 set -euxo pipefail
 
-TARGET=x86_64-unknown-linux-gnu
+TARGET=$(rustc -vV | sed -n 's|host: ||p')
+BIN=lunar
+TARGET_CPU=native
 
-# which llvm-profdata
-PROFDATA=~/.rustup/toolchains/nightly-2023-10-16-$TARGET/lib/rustlib/$TARGET/bin/llvm-profdata
+cargo pgo -V
+cargo pgo build -- --bin "$BIN"
 
-# STEP 0: Make sure there is no left-over profiling data from previous runs
-rm -rf /tmp/pgo-data
+cp target/$TARGET/release/$BIN target/release/"$BIN"_pgo_instrumented
 
-# STEP 1: Build the instrumented binaries
-RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" \
-    cargo build --release
+export LLVM_PROFILE_FILE=$PWD/target/pgo-profiles/"$BIN"_%m_%p.profraw
 
-cp target/release/lunar target/release/lunar_no_pgo
-
-# STEP 2: Run the instrumented binaries with some typical data
 ./scripts/c_chess_cli.py \
-  -engine cmd=./target/release/lunar_no_pgo tc=1+.1 name="lunar no pgo" \
-  -engine cmd=./target/release/lunar_no_pgo tc=1+.1 name="lunar no pgo" \
+  -engine cmd=target/release/"$BIN"_pgo_instrumented tc=1+.1 name="$BIN no pgo" \
+  -engine cmd=target/release/"$BIN"_pgo_instrumented tc=1+.1 name="$BIN no pgo" \
   -draw number=40 count=8 score=10 \
   -resign count=3 score=500 \
   -games 20 \
@@ -29,22 +25,6 @@ cp target/release/lunar target/release/lunar_no_pgo
   2> /dev/null
 
 
-# STEP 3: Merge the `.profraw` files into a `.profdata` file
-$PROFDATA merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
-
-# STEP 4: Use the `.profdata` file for guiding optimizations
-RUSTFLAGS="-Cprofile-use=/tmp/pgo-data/merged.profdata -Ctarget-cpu=native" \
-    cargo build --release
-
-cp target/release/lunar target/release/lunar_pgo
-
-# ./scripts/c_chess_cli.py \
-#   -engine cmd=./target/$TARGET/release/lunar_pgo tc=1+.1 name="lunar with pgo" \
-#   -engine cmd=./target/$TARGET/release/lunar_no_pgo tc=1+.1 name="lunar no pgo" \
-#   -draw number=40 count=8 score=10 \
-#   -resign count=3 score=500 \
-#   -games 500 \
-#   -repeat \
-#   -concurrency 2 \
-#   -openings file=./test_data/blitz_openings.fen order=random \
-#   2> /dev/null
+RUSTFLAGS="-Ctarget-cpu=$TARGET_CPU" \
+  cargo pgo optimize build -- --bin "$BIN"
+cp target/$TARGET/release/lunar target/release/"$BIN"_pgo
