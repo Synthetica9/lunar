35a103281d4fe39ab7a795b325db1bd351db57c6
Patrick Hilhorst <git@hilhorst.be>
2023-11-02 16:52:01 +0100

    ensure target dir exists before copying

--- scripts/c_chess_cli.py
+++ scripts/c_chess_cli.py
@@ -23,6 +23,7 @@ def setup_chess_cli():
             cwd=d,
         )
         subprocess.check_call(["python", "./make.py"], cwd=p)
+        TARGET_DIR.mkdir(exists_ok=True)
         shutil.copy(p / "c-chess-cli", OUT_FILE)
         print("Done compiling", file=sys.stderr)
 
