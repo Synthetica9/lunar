8b3f1158db07d8ecb3047a8e6f083d9c213bf276
Patrick Hilhorst <git@hilhorst.be>
2024-01-23 20:22:57 +0000

    improve grab script and parse script

--- scripts/grab_pgns.sh
+++ scripts/grab_pgns.sh
@@ -1,17 +1,14 @@
-rm concat.pgn
-
 mkdir -p pgns
 
 gh run list --limit 1000 --json 'workflowName,databaseId' \
   --jq '.[] | select(.workflowName == "Self-Play") | .databaseId' | \
   while read -r line; do
-    TEMP="$(mktemp -d)"
     [[ -f "pgns/$line.pgn" ]] && continue
+    TEMP="$(mktemp -d)"
     echo "Grabbing $line.pgn"
-    gh run download -n pgn -D "$TEMP" $line || touch $TEMP/out.pgn
-    mv "$TEMP/out.pgn" "$TEMP/$line.pgn" || true
-    mv "$TEMP/*.pgn" pgns/
+    gh run download -D "$TEMP" $line
+    find "$TEMP" -name '*.pgn' -exec cat {} \; > pgns/$line.pgn
     rm -r "$TEMP"
   done
 
-cat out.pgn pgns/*.pgn >> concat.pgn
+cat out.pgn pgns/*.pgn > concat.pgn
--- scripts/total_elos.sh
+++ scripts/total_elos.sh
@@ -1,16 +1,39 @@
+#!/usr/bin/env bash
+
 set -euxo pipefail
+HEAD=$(git log --pretty='format:%h' -1 @ -- src parameter.yaml cargo.toml cargo.lock scripts/build_pgo.sh)
 
 HERE=$(pwd)
-HEAD=$(git log --pretty='format:%h' -1 @ -- src parameter.yaml cargo.toml cargo.lock scripts/build_pgo.sh)
-cd $(mktemp -d)
+TEMP=$(mktemp -d)
+
+pushd $TEMP
 
 bayeselo << EOL
   prompt off
   readpgn $HERE/concat.pgn
-  players >players
-  ! grep $HEAD players | awk '{ print \$1 }' >head_player
-  ! cat head_player
-  connect <head_player
+  players>players
+EOL
+
+popd
+
+comm -1 -2 \
+  <(awk '{ print $3 }' $TEMP/players | sort) \
+  <(./scripts/relevant_commits.sh HEAD | sort) \
+  | xargs git rev-list --topo-order --no-walk \
+  | head -n 1 \
+  | xargs git rev-parse --short \
+  > $TEMP/head_player
+
+pushd $TEMP
+HEAD_HASH=$(cat head_player)
+echo $HEAD_HASH
+
+HEAD_PLAYER=$(grep "lunar $HEAD_HASH" players | head -n 1 | awk '{ print $1 }' )
+
+bayeselo << EOL
+  prompt off
+  readpgn $HERE/concat.pgn
+  connect $HEAD_PLAYER
   elo
   mm
   exactdist
