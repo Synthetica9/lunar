dfdae7a3b85163defb36795ed315515198d3119a
Patrick Hilhorst <git@hilhorst.be>
2025-03-20 17:32:15 +0000

    hugepagesalloc: fallback to regular when needed

--- src/hugepages_mmap_alloc.rs
+++ src/hugepages_mmap_alloc.rs
@@ -9,21 +9,49 @@ use nix::sys::mman::{mmap, munmap, MapFlags, ProtFlags};
 
 pub struct HugePagesAlloc;
 
+#[derive(Debug)]
+enum HugePagesAllocFailure {
+    MMapErr(nix::errno::Errno),
+    Null,
+}
+
+fn alloc(size: NonZeroUsize, with_hugepages: bool) -> Result<NonNull<[u8]>, HugePagesAllocFailure> {
+    let prot_flags = ProtFlags::PROT_READ | ProtFlags::PROT_WRITE;
+
+    let map_flags = {
+        let mut flags = MapFlags::MAP_PRIVATE | MapFlags::MAP_ANONYMOUS;
+        if with_hugepages {
+            flags |= MapFlags::MAP_HUGETLB;
+        }
+        flags
+    };
+    let f: Option<File> = None;
+    let mmap_result = unsafe { mmap(None, size, prot_flags, map_flags, f, 0) }
+        .map_err(|err| HugePagesAllocFailure::MMapErr(err))?;
+    let slc = slice_from_raw_parts_mut(mmap_result.cast::<u8>(), size.get());
+    let nonnull = NonNull::new(slc).ok_or(HugePagesAllocFailure::Null)?;
+    Ok(nonnull)
+}
+
 unsafe impl Allocator for HugePagesAlloc {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let length = NonZeroUsize::new(layout.size()).ok_or(AllocError)?;
+        let size = NonZeroUsize::new(layout.size()).ok_or(AllocError)?;
 
-        let prot_flags = ProtFlags::PROT_READ | ProtFlags::PROT_WRITE;
-        let map_flags = MapFlags::MAP_PRIVATE | MapFlags::MAP_ANONYMOUS | MapFlags::MAP_HUGETLB;
-        let f: Option<File> = None;
-        let mmap_result =
-            unsafe { mmap(None, length, prot_flags, map_flags, f, 0) }.map_err(|err| {
-                eprintln!("mmap: {err:?}");
-                AllocError
-            })?;
-        let slc = slice_from_raw_parts_mut(mmap_result.cast::<u8>(), length.get());
-        let nonnull = NonNull::new(slc).ok_or(AllocError)?;
-        Ok(nonnull)
+        for with_hugepages in [true, false] {
+            match alloc(size, with_hugepages) {
+                Err(_) if with_hugepages => {
+                    println!("Could not allocate with hugepages, falling back to regular");
+                    continue;
+                }
+                x => {
+                    return x.map_err(|err| {
+                        println!("{err:?}");
+                        AllocError {}
+                    })
+                }
+            }
+        }
+        Err(AllocError)
     }
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
         munmap(ptr.as_ptr().cast(), layout.size()).expect("munmap failed");
