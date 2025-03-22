48cebf37f5998adfd4f97843b40d3c71eea4de6b
Patrick Hilhorst <git@hilhorst.be>
2025-03-22 20:15:12 +0000

    Better total_elos output

--- scripts/total_elos.sh
+++ scripts/total_elos.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
-HEAD=$(git log --pretty='format:%h' -1 @ -- src parameter.yaml cargo.toml cargo.lock scripts/build_pgo.sh)
+HEAD=$(git log --pretty='format:%h' -1 @~ -- src parameter.yaml cargo.toml cargo.lock scripts/build_pgo.sh)
 
-HERE=$(pwd)
+export HERE=$(pwd)
 TEMP=$(mktemp -d)
 
 pushd $TEMP
@@ -18,7 +18,7 @@ popd
 
 comm -1 -2 \
   <(awk '{ print $3 }' $TEMP/players | sort) \
-  <(./scripts/relevant_commits.sh HEAD | sort) \
+  <(./scripts/relevant_commits.sh $HEAD | sort) \
   | xargs git rev-list --topo-order --no-walk \
   | head -n 1 \
   | xargs git rev-parse --short \
@@ -37,5 +37,31 @@ bayeselo << EOL
   elo
   mm
   exactdist
-  ratings
+  ratings>ratings
 EOL
+
+python -c '
+import re
+import subprocess
+import os
+ratings = open("ratings").read()
+git_dir = ["--git-dir", os.environ["HERE"] + "/.git"]
+
+lunar_re = re.compile(r"lunar (\w+)")
+descs = {}
+for m in lunar_re.finditer(ratings):
+  hash = m[1]
+  desc = subprocess.check_output(["git", *git_dir, *"describe --contains --all --always".split(), hash]).decode("utf8").strip()
+  descs[hash] = desc
+
+for (hash, desc) in descs.items():
+  ratings = ratings.replace(hash, desc)
+
+starts = [m.rstrip() for m in re.findall("^(.+):", ratings, flags=re.MULTILINE)]
+fill = max(map(len, starts))
+
+for start in starts:
+  ratings = ratings.replace(start, start.ljust(fill, " "))
+
+print(ratings)
+'
