96a1fd11203066fe5f17ff625a0127d106bbba2e
Patrick Hilhorst <git@hilhorst.be>
2025-05-28 17:15:09 +0200

    SCReLU128 arch, Lizard SCReLU

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -21,7 +21,7 @@ This is how you would load the network in rust.
 Commented out because it will error if it can't find the file.
 */
 pub static NNUE: Network =
-    unsafe { std::mem::transmute(*include_bytes!("../../nets/simple128.nnue")) };
+    unsafe { std::mem::transmute(*include_bytes!("../../nets/screlu128.nnue")) };
 
 pub fn evaluation(game: &Game) -> Millipawns {
     let (us, them) = match game.to_move() {
@@ -32,10 +32,11 @@ pub fn evaluation(game: &Game) -> Millipawns {
 }
 
 #[inline]
-/// Clipped ReLU - Activation Function.
+/// Squared Clipped ReLU - Activation Function.
 /// Note that this takes the i16s in the accumulator to i32s.
-fn crelu(x: i16) -> i32 {
-    i32::from(x).clamp(0, i32::from(QA))
+fn screlu(x: i16) -> i32 {
+    let clipped = i32::from(x).clamp(0, i32::from(QA));
+    clipped * clipped
 }
 
 /// This is the quantised format that bullet outputs.
@@ -57,18 +58,85 @@ impl Network {
     /// Calculates the output of the network, starting from the already
     /// calculated hidden layer (done efficiently during makemoves).
     pub fn evaluate(&self, us: &Accumulator, them: &Accumulator) -> i32 {
+        // Generic implementation
+        let acc_sum = || {
+            let mut res = 0;
+
+            // Side-To-Move Accumulator -> Output.
+            for (&input, &weight) in us.vals.iter().zip(&self.output_weights[..HIDDEN_SIZE]) {
+                res += screlu(input) * i32::from(weight);
+            }
+
+            // Not-Side-To-Move Accumulator -> Output.
+            for (&input, &weight) in them.vals.iter().zip(&self.output_weights[HIDDEN_SIZE..]) {
+                res += screlu(input) * i32::from(weight);
+            }
+
+            res
+        };
+
+        #[cfg(all(target_arch = "x86_64", feature = "asm"))]
+        let acc_sum = || {
+            unsafe {
+                // https://www.chessprogramming.org/NNUE#Lizard_SCReLU
+                use std::arch::x86_64::*;
+
+                const SUM_SIZE: usize = 256 / 16;
+
+                let zero = _mm256_setzero_si256();
+                let qa = _mm256_set1_epi16(QA);
+                let mut sum = zero;
+
+                let us_ptr = us.vals.as_ptr().cast::<__m256i>();
+                let them_ptr = them.vals.as_ptr().cast::<__m256i>();
+                let us_weights_ptr = self.output_weights.as_ptr().cast::<__m256i>();
+                let them_weights_ptr = self
+                    .output_weights
+                    .as_ptr()
+                    .add(HIDDEN_SIZE)
+                    .cast::<__m256i>();
+
+                for i in 0..(HIDDEN_SIZE / SUM_SIZE) {
+                    let us = _mm256_load_si256(us_ptr.add(i));
+                    let them = _mm256_load_si256(them_ptr.add(i));
+                    let us_weights = _mm256_load_si256(us_weights_ptr.add(i));
+                    let them_weights = _mm256_load_si256(them_weights_ptr.add(i));
+
+                    let us_clamped = _mm256_min_epi16(_mm256_max_epi16(us, zero), qa);
+                    let them_clamped = _mm256_min_epi16(_mm256_max_epi16(them, zero), qa);
+
+                    let us_results =
+                        _mm256_madd_epi16(_mm256_mullo_epi16(us_weights, us_clamped), us_clamped);
+                    let them_results = _mm256_madd_epi16(
+                        _mm256_mullo_epi16(them_weights, them_clamped),
+                        them_clamped,
+                    );
+
+                    sum = _mm256_add_epi32(sum, us_results);
+                    sum = _mm256_add_epi32(sum, them_results);
+                }
+
+                // sum contains 8 * 32 bits, so 3 shuffles:
+                for _i in 0..2 {
+                    sum = _mm256_hadd_epi32(sum, sum);
+                }
+
+                // But _mm256_hadd_epi32 doesn't cross the 128 bit mark,
+                // so we have to do the last shuffle manually:
+
+                let lower = _mm256_extract_epi32::<0>(sum);
+                let upper = _mm256_extract_epi32::<4>(sum);
+                lower + upper
+            }
+        };
+
         // Initialise output with bias.
-        let mut output = i32::from(self.output_bias);
+        let mut output = 0;
 
-        // Side-To-Move Accumulator -> Output.
-        for (&input, &weight) in us.vals.iter().zip(&self.output_weights[..HIDDEN_SIZE]) {
-            output += crelu(input) * i32::from(weight);
-        }
+        output += acc_sum();
+        output /= i32::from(QA);
 
-        // Not-Side-To-Move Accumulator -> Output.
-        for (&input, &weight) in them.vals.iter().zip(&self.output_weights[HIDDEN_SIZE..]) {
-            output += crelu(input) * i32::from(weight);
-        }
+        output += i32::from(self.output_bias);
 
         // Apply eval scale.
         output *= SCALE;
