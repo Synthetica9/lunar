5d47a4588f1d33db5c5d4451db1f9363489a5e85
Patrick Hilhorst <git@hilhorst.be>
2025-07-08 18:53:44 +0200

    tripple search

--- src/search/search_thread.rs
+++ src/search/search_thread.rs
@@ -2,6 +2,7 @@
 // See: https://web.archive.org/web/20220116101201/http://www.tckerrigan.com/Chess/Parallel_Search/Simplified_ABDADA/simplified_abdada.html
 
 use std::cell::Cell;
+use std::i32;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
@@ -772,25 +773,24 @@ impl ThreadData {
 
                 self.history.push(ply);
 
-                let x = if is_first_move {
-                    // What else could we be overwriting here?
-                    // if let Some(best) = best_move {
-                    //     debug_assert_eq!(best, ply);
-                    // }
+                // Null-window search
+                let mut x = Millipawns(i32::MIN + 1234);
 
+                if is_first_move && !N::is_pv() {
+                    // XXX: Highly dubious, but gotta test removal seperately.
                     best_move = Some(ply);
-                    -self
-                        .alpha_beta_search::<N::FirstSuccessor>(
-                            -beta,
+
+                    x = -self
+                        .alpha_beta_search::<N::OtherSuccessors>(
+                            -alpha - Millipawns::ONE,
                             -alpha,
                             full_depth,
                             root_dist + 1,
                         )?
                         .0
-                } else {
-                    // Null-window search
-                    let mut x = -self
-                        .alpha_beta_search::<N::OtherSuccessors>(
+                } else if !is_first_move {
+                    x = -self
+                        .alpha_beta_search::<CutNode>(
                             -alpha - Millipawns::ONE,
                             -alpha,
                             next_depth,
@@ -798,16 +798,35 @@ impl ThreadData {
                         )?
                         .0;
 
-                    if x > alpha && (x < beta || is_reduced) {
+                    if is_reduced && x > alpha {
                         x = -self
-                            .alpha_beta_search::<PVNode>(-beta, -alpha, full_depth, root_dist + 1)?
+                            .alpha_beta_search::<N::OtherSuccessors>(
+                                -alpha - Millipawns::ONE,
+                                -alpha,
+                                full_depth,
+                                root_dist + 1,
+                            )?
                             .0;
                     }
-
-                    x
                 };
 
+                // XXX: investigate x < beta condition.
+                if N::is_pv() && (is_first_move || (x > alpha && x < beta)) {
+                    debug_assert!(beta - alpha > Millipawns(1), "{beta:?} {alpha:?}");
+                    x = -self
+                        .alpha_beta_search::<PVNode>(-beta, -alpha, full_depth, root_dist + 1)?
+                        .0
+                };
+
+                debug_assert_ne!(x, Millipawns(i32::MIN + 1234));
+
                 let undo = self.history.pop();
+
+                value = value.max(x);
+                if value > alpha {
+                    alpha = value;
+                    best_move = Some(ply);
+                }
 
                 if N::IS_ROOT {
                     let total_nodes_after = self.total_nodes_searched;
@@ -816,12 +835,6 @@ impl ThreadData {
                     let atom = &self.root_move_counts[&ply];
                     atom.fetch_add(searched, Ordering::Relaxed);
                     *self.curr_ply_root_move_counts.get_mut(&ply).unwrap() += searched as u64;
-                }
-
-                value = value.max(x);
-                if value > alpha {
-                    alpha = value;
-                    best_move = Some(ply);
                 }
 
                 if alpha >= beta {
