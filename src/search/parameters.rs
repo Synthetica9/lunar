e7c9747a57dea2520bc77c56e3f56e79983ed6aa
Patrick Hilhorst <git@hilhorst.be>
2025-07-31 08:41:25 +0200

    Smol tune intermediate results
    bench: 9253995

--- src/search/parameters.rs
+++ src/search/parameters.rs
@@ -152,66 +152,66 @@ macro_rules! make_tunable_params {
 make_tunable_params! {
     // Name: type = value, min, max, C_end, R_end
     // https://github.com/AndyGrant/OpenBench/wiki/SPSA-Tuning-Workloads
-    nmp_offset: Depth = 2., 0., 6., 0.3, 0.002;
-    nmp_piece_slope: Depth = 0.1, 0., 0.5, 0.025, 0.002;
-    nmp_depth_slope: Depth = 1. / 7., 0., 0.5, 0.025, 0.002;
+    nmp_offset: Depth = 2.157943528009613, 0., 6., 0.3, 0.002;
+    nmp_piece_slope: Depth = 0.08651773875486138, 0., 0.5, 0.025, 0.002;
+    nmp_depth_slope: Depth = 0.16347959223127906, 0., 0.5, 0.025, 0.002;
 
-    iir_reduction: Depth = 2., 0., 5., 0.25, 0.002;
-    iir_min_depth: Depth = 6., 3., 12., 0.45, 0.002;
-    iir_tt_limit: Depth = 3., 0., 6., 0.2, 0.002;
+    iir_reduction: Depth = 1.9406457478286816, 0., 5., 0.25, 0.002;
+    iir_min_depth: Depth = 6.513250830427466, 3., 12., 0.45, 0.002;
+    iir_tt_limit: Depth = 2.866307165506535, 0., 6., 0.2, 0.002;
 
-    lmr_quiescent_slope: Depth = 0.12, 0., 2., 0.1, 0.002;
-    lmr_quiescent_offset: Depth = 1.41, 0., 3., 0.15, 0.002;
-    lmr_quiet_slope: Depth = 0.36, 0., 2., 0.1, 0.002;
-    lmr_quiet_offset: Depth = 1.5, 0., 3., 0.15, 0.002;
-    lmr_improving_rate: Depth = 0.25, 0., 1., 0.05, 0.002;
+    lmr_quiescent_slope: Depth = 0.03433192631399915, 0., 2., 0.1, 0.002;
+    lmr_quiescent_offset: Depth = 1.220215888885144, 0., 3., 0.15, 0.002;
+    lmr_quiet_slope: Depth = 0.3299256419073334, 0., 2., 0.1, 0.002;
+    lmr_quiet_offset: Depth = 0.9780167770388664, 0., 3., 0.15, 0.002;
+    lmr_improving_rate: Depth = 0.14389521778518116, 0., 1., 0.05, 0.002;
 
-    check_extension: Depth = 0.5, 0., 2., 0.1, 0.002;
-    neg_see_reduction: Depth = 0.5, 0., 2., 0.1, 0.002;
-    mate_threat_extension: Depth = 0.5, 0., 2., 0.1, 0.002;
+    check_extension: Depth = 0.38140019929963487, 0., 2., 0.1, 0.002;
+    neg_see_reduction: Depth = 0.6297848596161078, 0., 2., 0.1, 0.002;
+    mate_threat_extension: Depth = 0.47986308413664136, 0., 2., 0.1, 0.002;
 
-    futprun_max_depth: Depth = 6.17, 3., 10., 0.35, 0.002;
-    futprun_mp_per_ply: Depth = 384., 0., 1000., 50., 0.002;
-    futprun_min_mp: i32 = 0, 0, 1000, 50., 0.002;
+    futprun_max_depth: Depth = 6.354615218136067, 3., 10., 0.35, 0.002;
+    futprun_mp_per_ply: Depth = 571.2163796572776, 0., 1000., 50., 0.002;
+    futprun_min_mp: i32 = 9, 0, 1000, 50., 0.002;
 
-    tt_capture_reduction: Depth = 0.5, 0., 2., 0.1, 0.002;
+    tt_capture_reduction: Depth = 0.5918770254854429, 0., 2., 0.1, 0.002;
 
-    mo_continuation_start_weight:  Depth = 40., 0., 100., 5., 0.002;
-    mo_continuation_factor: Depth = 0.75, 0., 1., 0.05, 0.002;
-    mo_direct_history_weight: i32 = 50, 0, 100, 5., 0.002;
-    mo_move_threatened_piece_bonus: i32 = 1000, 0, 3000, 150., 0.002;
-    mo_nmp_threat_min_sevr: i32 = 100, 0, 2000, 30., 0.002;
-    mo_sevr_scaling_max: i32 = 2500, 0, 10000, 200., 0.002;
-    mo_sevr_move_threat: i32 = 50, 0, 200, 5., 0.002;
+    mo_continuation_start_weight:  Depth = 44.045077506372664, 0., 100., 5., 0.002;
+    mo_continuation_factor: Depth = 0.7506528958448074, 0., 1., 0.05, 0.002;
+    mo_direct_history_weight: i32 = 40, 0, 100, 5., 0.002;
+    mo_move_threatened_piece_bonus: i32 = 1305, 0, 3000, 150., 0.002;
+    mo_nmp_threat_min_sevr: i32 = 143, 0, 2000, 30., 0.002;
+    mo_sevr_scaling_max: i32 = 2591, 0, 10000, 200., 0.002;
+    mo_sevr_move_threat: i32 = 41, 0, 200, 5., 0.002;
 
     aw_min_depth: i32 = 5, 2, 10, 0.5, 0.002;
-    aw_base_window: f64 = 500., 0., 1000., 50., 0.002;
-    aw_widening_base: f64 = 1., 0., 3., 0.15, 0.002;
+    aw_base_window: f64 = 340.29425793210805, 0., 1000., 50., 0.002;
+    aw_widening_base: f64 = 0.6995966795724402, 0., 3., 0.15, 0.002;
     aw_fail_open_after: i32 = 5, 0, 15, 0.75, 0.002;
-    aw_consistency_base: f64 = 0.9, 0.5, 1., 0.025, 0.002;
+    aw_consistency_base: f64 = 0.90597022904253, 0.5, 1., 0.025, 0.002;
 
-    rfp_depth_slope: Depth = 1500., 0., 4000., 200., 0.002;
-    rfp_improving_fac: Depth = 0.5, 0., 1., 0.05, 0.002;
-    rfp_min_depth: Depth = 4., 0., 12., 0.5, 0.002;
+    rfp_depth_slope: Depth = 930.0923973641561, 0., 4000., 200., 0.002;
+    rfp_improving_fac: Depth = 0.5616900049641566, 0., 1., 0.05, 0.002;
+    rfp_min_depth: Depth = 4.611616158400307, 0., 12., 0.5, 0.002;
 
-    see_pruning_noisy_scaling_factor: Depth = -500., -2000., 0., 100., 0.002;
-    see_pruning_quiet_scaling_factor: Depth = -800., -2000., 0., 100., 0.002;
+    see_pruning_noisy_scaling_factor: Depth = -370.0662456844208, -2000., 0., 100., 0.002;
+    see_pruning_quiet_scaling_factor: Depth = -683.4333685655881, -2000., 0., 100., 0.002;
 
-    lmp_depth_slope: Depth = 2., 0., 5., 0.25, 0.002;
-    lmp_offset: Depth = 5., 0., 15., 1., 0.002;
+    lmp_depth_slope: Depth = 1.72957188515299, 0., 5., 0.25, 0.002;
+    lmp_offset: Depth = 3.339359102199104, 0., 15., 1., 0.002;
 
-    histprun_depth_scale: Depth = -4000., -10000., 0., 500., 0.002;
-    histprun_offset: i32 = -500, -2000, 0, 100.0, 0.002;
+    histprun_depth_scale: Depth = -3456.2560120496832, -10000., 0., 500., 0.002;
+    histprun_offset: i32 = -356, -2000, 0, 100.0, 0.002;
 
-    se_tt_offset: Depth = 3., 0., 6., 0.25, 0.002;
-    se_min_depth: Depth = 8., 3., 12., 0.4, 0.002;
-    se_beta_scaling: Depth = 20., 0., 100., 5., 0.002;
-    se_depth_scaling: Depth = 0.5, 0., 1., 0.05, 0.002;
-    se_double_ext_margin: i32 = 200, 0, 500, 10., 0.002;
-    se_ext: Depth = 1., 0., 2., 0.1, 0.002;
-    se_double_ext: Depth = 1., 0., 2., 0.1, 0.002;
+    se_tt_offset: Depth = 3.485648583086322, 0., 6., 0.25, 0.002;
+    se_min_depth: Depth = 6.539047903924174, 3., 12., 0.4, 0.002;
+    se_beta_scaling: Depth = 21.20828202312381, 0., 100., 5., 0.002;
+    se_depth_scaling: Depth = 0.4974783172493165, 0., 1., 0.05, 0.002;
+    se_double_ext_margin: i32 = 202, 0, 500, 10., 0.002;
+    se_ext: Depth = 0.9289110668077334, 0., 2., 0.1, 0.002;
+    se_double_ext: Depth = 0.9365869398471875, 0., 2., 0.1, 0.002;
 
-    lmpahp_cutoff_depth: Depth = -2., -4., 0., 0.2, 0.002;
+    lmpahp_cutoff_depth: Depth = -2.0846465770501754, -4., 0., 0.2, 0.002;
 }
 
 #[cfg(feature = "tunable")]
