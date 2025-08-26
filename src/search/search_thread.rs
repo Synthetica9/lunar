b535e46b0a378ecd980266f33644783dca17bebb
Patrick Hilhorst <git@hilhorst.be>
2025-08-26 21:01:25 +0200

    remove double nmp check
    bench: 14563150

--- src/search/search_thread.rs
+++ src/search/search_thread.rs
@@ -673,13 +673,7 @@ impl ThreadData {
 
             let mut is_mate_threat = false;
 
-            if N::is_cut()
-                && !N::IS_SE
-                && !side_to_move_only_kp
-                && depth >= r
-                && !is_in_check
-                && !self.history.last_is_null()
-            {
+            if N::is_cut() && !N::IS_SE && !side_to_move_only_kp && depth >= r && !is_in_check {
                 self.history.push(Ply::NULL);
                 let null_res = self.alpha_beta_search::<CutNode>(
                     -beta,
