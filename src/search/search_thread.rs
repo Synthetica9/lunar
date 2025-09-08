d1f6311c896b165b3d655749ad300914076fd184
Patrick Hilhorst <git@hilhorst.be>
2025-09-08 06:46:03 +0200

    4-fold continuation histories
    bench: 13727975

--- src/search/search_thread.rs
+++ src/search/search_thread.rs
@@ -28,7 +28,8 @@ use crate::transposition_table::TranspositionTable;
 use crate::zero_init::ZeroInit;
 use crate::zobrist_hash::ZobristHash;
 
-pub const N_CONTINUATION_HISTORIES: usize = 2;
+pub const N_CONTINUATION_HISTORIES: usize = 3;
+pub const CONTINUATION_HISTORIES: [usize; N_CONTINUATION_HISTORIES] = [0, 1, 3];
 const COMMS_INTERVAL: usize = 1 << 13;
 
 pub const MAX_CORR_HIST: Millipawns = Millipawns(1024);
@@ -225,7 +226,7 @@ impl HistoryTables {
 
         let cont_weights = continuation_weights();
 
-        for i in 0..N_CONTINUATION_HISTORIES {
+        for (weight, i) in cont_weights.into_iter().zip(CONTINUATION_HISTORIES) {
             let Some(oppt_info) = stack.peek_n(i) else {
                 continue;
             };
@@ -236,7 +237,7 @@ impl HistoryTables {
 
             self.continuation.update_cell(
                 (oppt_info.color_piece_dst(), (color, piece, ply.dst())),
-                |x| f(x, Depth::from_num(cont_weights[i])),
+                |x| f(x, Depth::from_num(weight)),
             );
         }
 
