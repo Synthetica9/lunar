bb5aaa6c9ee380173247ef9dbc7154e416aec008
Patrick Hilhorst <git@hilhorst.be>
2025-09-10 19:57:05 +0000

    warmup training
    bench: 16034500

--- trainer/src/main.rs
+++ trainer/src/main.rs
@@ -13,9 +13,15 @@ fn main() {
     // hyperparams to fiddle with
     let hl_size = 1024;
     let dataset_path = "data/baseline.bin";
+    let warmup_path = "data/warmup.bin";
     let initial_lr = 0.001;
-    let final_lr = 0.001 * 0.3f32.powi(5);
-    let superbatches = 240;
+    let final_lr = initial_lr * 0.3f32.powi(5);
+
+    let start_wdl = 0.2;
+    let end_wdl = 0.5;
+
+    let warmup_superbatches = 10; // 3 epochs
+    let superbatches = 240; // 20 epochs
     let qa = 255;
     let qb = 64;
 
@@ -46,8 +52,37 @@ fn main() {
             l1.forward(hidden_layer)
         });
 
-    let schedule = TrainingSchedule {
-        net_id: "screlu512_hm".to_string(),
+    let settings = LocalSettings {
+        threads: 4,
+        test_set: None,
+        output_directory: "checkpoints",
+        batch_queue_size: 64,
+    };
+
+    let base_name = format!("screlu{hl_size}_hm");
+    let warmup_schedule = TrainingSchedule {
+        net_id: format!("{base_name}_warmup"),
+        eval_scale: scale as f32,
+        steps: TrainingSteps {
+            batch_size: 16_384,
+            batches_per_superbatch: 6104,
+            start_superbatch: 1,
+            end_superbatch: warmup_superbatches,
+        },
+        wdl_scheduler: wdl::LinearWDL {
+            start: 0.0,
+            end: start_wdl,
+        },
+        lr_scheduler: lr::ConstantLR { value: initial_lr },
+        save_rate: 10,
+    };
+
+    let dataloader = DirectSequentialDataLoader::new(&[warmup_path]);
+
+    trainer.run(&warmup_schedule, &settings, &dataloader);
+
+    let main_schedule = TrainingSchedule {
+        net_id: base_name,
         eval_scale: scale as f32,
         steps: TrainingSteps {
             batch_size: 16_384,
@@ -56,8 +91,8 @@ fn main() {
             end_superbatch: superbatches,
         },
         wdl_scheduler: wdl::LinearWDL {
-            start: 0.0,
-            end: 0.5,
+            start: start_wdl,
+            end: end_wdl,
         },
         lr_scheduler: lr::CosineDecayLR {
             initial_lr,
@@ -67,14 +102,7 @@ fn main() {
         save_rate: 10,
     };
 
-    let settings = LocalSettings {
-        threads: 4,
-        test_set: None,
-        output_directory: "checkpoints",
-        batch_queue_size: 64,
-    };
-
     let dataloader = DirectSequentialDataLoader::new(&[dataset_path]);
 
-    trainer.run(&schedule, &settings, &dataloader);
+    trainer.run(&main_schedule, &settings, &dataloader);
 }
